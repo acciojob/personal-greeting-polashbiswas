3405bb3bc5acb8e2e8bf08887a455c2df879cdab
Polash Biswas <polashbiswasit19@gmail.com>
2023-06-10 19:05:03 +0530

    submitting code-Sat Jun 10 2023 19:05:01 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [name, setName] = useState("");
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text"
+        onChange={(e) => setName(e.target.value)} 
+        />
+        {
+          name && 
+          <p>Hello {name}!</p>
+        }
     </div>
   )
 }
